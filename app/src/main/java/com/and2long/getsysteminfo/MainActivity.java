aaa7807243496c6547b20fa6abc01a8dc657666e
and2long <and2long@gmail.com>
2016-11-24 20:40:07 +0800

    Initial commit

---
+++ app/src/main/java/com/and2long/getsysteminfo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.and2long.getsysteminfo;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvIP;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle(getString(R.string.activity_system_info));
+
+        tvIP = (TextView) findViewById(R.id.tv_ip);
+
+        getIP();
+
+    }
+
+    /**
+     * 获取ip地址
+     */
+    private void getIP() {
+        //获取wifi服务
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        //判断wifi是否开启
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipAddress = wifiInfo.getIpAddress();
+        String ip = intToIp(ipAddress);
+        tvIP.setText("IP地址：" + ip);
+    }
+
+
+    private String intToIp(int i) {
+        return (i & 0xFF) + "." +
+                ((i >> 8) & 0xFF) + "." +
+                ((i >> 16) & 0xFF) + "." +
+                (i >> 24 & 0xFF);
+    }
+}
